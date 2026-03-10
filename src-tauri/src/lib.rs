8b12c61ac6846ce5998fe6668cb1f611894cff8b
Sebastian DeClaw <264897834+sebastian-claw@users.noreply.github.com>
2026-03-10 08:25:18 -0400

    config: update to Tailscale IP 100.123.154.91

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,19 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            // Enable Cmd+R / Ctrl+R / F5 to reload
+            window.on_window_event(|_| {});
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::KeyboardInput { event: key_event, .. } = event {
+                let _ = (window, key_event);
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
